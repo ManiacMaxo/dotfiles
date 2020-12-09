09828b918150d1c5dfe82b534cf992607ef2947f
Victor <victor@gorchilov.com>
2020-12-09 19:09:51 +0200

    [up] add nvm and brew update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,16 +1,29 @@
 set fish_greeting
 
 #fun
-# alias starwars="nc towel.blinkenlights.nl 23" #outdated
+alias starwars "nc towel.blinkenlights.nl 23"
 
 #workflow
-alias gcce="gcc -Wall -pedantic -std=c11"
-alias server="ssh gorchilov.net"
-alias pip_update="pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U"
-alias jn="jupyter notebook"
+
+##develop
+alias gcce "gcc -Wall -pedantic -std=c11"
+alias pip_update "pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U"
+alias jn "jupyter notebook --ip 0.0.0.0"
+
+##time-saving
+alias server "ssh gorchilov.net"
+#alias boo "brew update && brew upgrade && brew upgrade --cask && brew cleanup && brew doctor"
+alias xa "exa -la"
+
 export PATH="/usr/local/sbin:$PATH"
 
 #git aliases
-alias gommit="git commit -m"
-alias gmain="git checkout main"
-alias gmas="git checkout master"
+alias gcm "git commit -m"
+alias gmain "git checkout main"
+alias gmas "git checkout master"
+
+function nvm
+   bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
+end
+
+nvm use default --silent
