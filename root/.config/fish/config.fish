fa38c97e2c0bcb3c68cea130a9be1e038333b6f9
Victor Gorchilov <victor@gorchilov.com>
2021-02-20 18:43:01 +0200

    git commit change date alias

--- root/.config/fish/config.fish
+++ root/.config/fish/config.fish
@@ -14,7 +14,7 @@ function cra
     npx create-react-app $argv --template typescript-scss
 end
 
-export PATH="/usr/local/sbin:$PATH"
+set PATH $PATH /usr/local/sbin 
 
 # git aliases
 function gcm
@@ -22,17 +22,3 @@ function gcm
 end
 alias gmain "git checkout main"
 alias gmas "git checkout master"
-
-if test (uname) = Darwin
-    alias boo "brew update && brew upgrade && brew upgrade --cask && brew cleanup && brew doctor"
-else if test (uname) = Linux;
-    if test (omf list | grep bass)
-        function nvm
-            bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
-        end
-        nvm use default --silent
-    else
-        echo "Install omf and bass to use nvm"
-    end
-    alias boo "sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove && sudo apt autoclean"
-end
